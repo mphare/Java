771b446db8abd53c7692995744242975c38ddb9e
Michael Hare <mphare@whitehare.com>
2014-08-17 21:51:29 -0500

    Fixed log4j logger

--- MVC/src/com/whitehare/mvc/MVCPatternDemo.java
+++ MVC/src/com/whitehare/mvc/MVCPatternDemo.java
@@ -1,13 +1,19 @@
 package com.whitehare.mvc;
 
+import org.apache.log4j.Logger;
+
 import com.whitehare.mvc.controller.StudentController;
 import com.whitehare.mvc.model.Student;
 import com.whitehare.mvc.view.StudentView;
 
 public class MVCPatternDemo
 {
+  private static Logger logger = Logger.getLogger(MVCPatternDemo.class);
+
   public static void main(String[] args)
   {
+
+    logger.debug("Getting Student from Database");
     Student model = retrieveStudentFromDatabase();
 
     StudentView view = new StudentView();
