f65486ec2f821d9a1a8265a6b6d046f45c0e34f0
Michael Hare <mphare@whitehare.com>
2014-08-17 20:30:31 -0500

    Added MVC sample project

---
+++ MVC/src/com/whitehare/mvc/MVCPatternDemo.java
@@ -0,0 +1,30 @@
+package com.whitehare.mvc;
+
+import com.whitehare.mvc.controller.StudentController;
+import com.whitehare.mvc.model.Student;
+import com.whitehare.mvc.view.StudentView;
+
+public class MVCPatternDemo
+{
+  public static void main(String[] args)
+  {
+    Student model = retrieveStudentFromDatabase();
+
+    StudentView view = new StudentView();
+
+    StudentController controller = new StudentController(model, view);
+
+    controller.updateView();
+    controller.setStudentName("John");
+    controller.updateView();
+  }
+
+  private static Student retrieveStudentFromDatabase()
+  {
+    Student student = new Student();
+    student.setName("Robert");
+    student.setRollNo("10");
+    return student;
+  }
+
+}
